379f7c29456d77896d2e999a278828c01f437444
abuhurairahbinamer <138894951+abuhurairahbinamer@users.noreply.github.com>
2026-08-11 10:39:20 +0500

    fix:P3 task done with deeper(Applied Ammendments)

--- assignment1/components/Navbar.tsx
+++ assignment1/components/Navbar.tsx
@@ -1,15 +1,17 @@
 'use client'
 import Link from 'next/link'
-
+import { useRef } from 'react'
 export default function Navbar() {
- 
+  const ref=useRef(0);
+  ref.current+=1;
+console.log(`P3 task done with deeper :navbar rendered ${ref.current} times`)
   return (
     <nav style={{ display: 'flex', gap: '20px', padding: '10px', background: '#eee' }}>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/contact">Contact</Link>
-      <Link href="/dashboard">Dashboard</Link>
-      <Link href="/practice">practice portion</Link>
+      <Link  className='border-2 border-transparent focus:border-blue-800 rounded-2xl' href="/">Home</Link>
+      <Link  className='border-2 border-transparent focus:border-blue-800 rounded-2xl' href="/about">About</Link>
+      <Link  className='border-2 border-transparent focus:border-blue-800 rounded-2xl' href="/contact">Contact</Link>
+      <Link  className='border-2 border-transparent focus:border-blue-800 rounded-2xl' href="/dashboard">Dashboard</Link>
+      <Link  className='border-2 border-transparent focus:border-blue-800 rounded-2xl' href="/practice">practice portion</Link>
     </nav>
   )
 }
