5698be5896c5ee623d551497d93e66dc36871655
abuhurairahbinamer <138894951+abuhurairahbinamer@users.noreply.github.com>
2026-08-11 15:35:31 +0500

    fix:p5 task done with deeper(Applied Ammendments)

--- assignment1/components/ErrorButton.tsx
+++ assignment1/components/ErrorButton.tsx
@@ -1,14 +1,17 @@
 'use client'
 
 import { useState } from 'react'
-
+import { usePathname } from 'next/navigation'
 export default function ErrorButton({message=null}:{message:string | null}) {
   const [shouldError, setShouldError] = useState(false)
-
+  const pathname=usePathname();
   if (shouldError) {
     throw new Error('Forced error from button click in Layout.tsx!')
   }
   const mess=message?message:"Force Error"
+  if(pathname!=='/practice'){
+    return ""
+  }
   return (
     <button className='p-3 bg-red-600 hover:bg-red-600/80 cursor-pointer rounded-2xl' onClick={() => setShouldError(true)}>
       {mess}
